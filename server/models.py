a8f8406bb5f8cf12576962aab3e0d6a0b4517cbb
crea-tivecoder <nyawirawaweru.com>
2024-11-01 16:46:20 +0300

    added validators to models

--- server/models.py
+++ server/models.py
@@ -12,7 +12,20 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
-
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Author name is required.")
+        if Author.query.filter_by(name=value).first():
+            raise ValueError("Author name must be unique.")
+        return value
+    
+    @validates('phone_number')
+    def validate_phone(self, key, number):
+        if not number or not (number.isdigit() and len(number) == 10):
+            raise ValueError("Phone number must be exactly 10 digits")
+        return number
+    
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -28,7 +41,27 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Content must be atleast 250 characters long")
+        return value
+    
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'")
+        return value
+    
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Summary is a maximum of 250 characters")
 
-
+    @validates('title')
+    def validate_title(self, key, value):
+        if not any(keyword in value for keyword in["Won't Believe", "Secret", "Top", "Guess"]):
+            raise ValueError("Title must contain one of the following: 'Secret', 'Top', or 'Guess'.")
+        return value
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
